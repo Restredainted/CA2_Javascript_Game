cb8a9e26a4b0909a3cab31677b716900f22dac9c
Restredainted <Anastasia.s.valkyrja@gmail.com>
2023-12-04 11:13:00 +0000

    Added terrain resource import, updated readme

--- js/engine/resources.js
+++ js/engine/resources.js
@@ -2,8 +2,12 @@
 const Images = {
 	player: new Image(), // The Image instance for the player.
 	enemy: new Image(), // The Image instance for the enemy.
-	coin: new Image(), // The Image instance for Gold Coins.
-	gem: new Image(), // The Image instance for Gems.
+	coin: new Image(), // The Image instance for gold coins.
+	gem: new Image(), // The Image instance for gems.
+	dirt: new Image(), // The image instance for dirt.
+	rock: new Image(), // The Image instance for rock.
+	veinGold: new Image(), // The Image instance for gold veins.
+	veinGem: new Image(), // The Image instance for gem veins. 
 };
 
 // Create an AudioFiles object to hold the file paths of the audio resources.
@@ -20,10 +24,22 @@ Images.player.src = './resources/images/player/player.png'; // Update the image 
 Images.enemy.src = './resources/images/enemy/enemy.png'; // Update the image path
 
 // Set the source of the gold coin.
-Images.coin.src = './resources/images/Collectables/SP_Coin.png'
+Images.coin.src = './resources/images/Collectables/SP_Coin.png';
 
 // Set the source of the Gem Image.
-Images.gem.src = './resources/images/Collectables/SP_Gem.png'
+Images.gem.src = './resources/images/Collectables/SP_Gem.png';
+
+// Set the source of the dirt Image.
+Images.dirt.src = './resources/images/Terrain/SP_Dirt.png';
+
+// Set the source of the Rock Image.
+Images.rock.src = './resources/images/Terrain/SP_Rock.png';
+
+// set the source of the gold vein Image.
+Images.veinGold.src = './resources/images/Terrain/SP_VeinGold.png';
+
+// Set the source of the gem vein Image.
+Images.veinGem.src = './resources/images/Terrain/SP_VeinGem.png';
 
 
 // Export the Images and AudioFiles objects so they can be imported and used in other modules.
