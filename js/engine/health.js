103c7956108033849f9dfde49c2b8896b0e24a79
Restredainted <Anastasia.s.valkyrja@gmail.com>
2023-12-12 03:34:36 +0000

    Started Health script

---
+++ js/engine/health.js
@@ -0,0 +1,22 @@
+import Component from "./component"
+
+class Health extends Component {
+
+    constructor(maxHP) {
+        this.maxHP = maxHP;
+        this.HP = maxHP;
+    }
+
+    damage(damage) {
+
+    }
+
+    heal(heal) {
+        
+    }
+
+}
+
+
+
+export default Health;
